3d83003cb44159226dad4625d284f110e16d6ef3
konomoha <konomoha@hotmail.fr>
2021-11-15 14:17:26 +0100

    dev layout page index + création fichier style

--- index.php
+++ index.php
@@ -12,18 +12,26 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-
-    <style>
-        body{
-            font-family: 'Roboto', sans-serif;
-        }
-    </style>
-
+    <link href="style.css" rel="stylesheet";>
     <title>Projet Moha</title>
   </head>
   <body>
-    <h1>Projet Mohahaha~</h1>
-    <p>ceci est un paragraphe</p>
+    <header>
+
+    <nav class="bg-warning p-4">
+        <ul class="row justify-content-around ">
+            <li class="col-md-2"><a href="" class="text-dark">Accueil</a></li>
+            <li class="col-md-2"><a href="" class="text-dark">produits</a></li>
+            <li class="col-md-2"><a href="" class="text-dark">Contact</a></li>
+            
+        </ul>
+    </nav>
+
+    </header>
+
+    <main>
+    <h1 class="text-center fst-italic">Projet Mohahaha~</h1>
+    </main>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
