2cce99a1278228babc76d9e2edbccea62423d5ee
konomoha <konomoha@hotmail.fr>
2021-11-15 15:06:20 +0100

    nouvelle mise à jour du site

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
   </head>
   <body>
     <header>
-
-    <nav class="bg-warning p-4">
+    
+    <nav >
         <ul class="row justify-content-around ">
             <li class="col-md-2"><a href="" class="text-dark">Accueil</a></li>
             <li class="col-md-2"><a href="" class="text-dark">produits</a></li>
@@ -30,7 +30,46 @@
     </header>
 
     <main>
-    <h1 class="text-center fst-italic">Projet Mohahaha~</h1>
+        <h1 class="text-center fst-italic">Projet Mohahaha~</h1>
+        <section>
+            <div id="test" class="col-md-5 mx-auto row justify-content-center"><img src="tee-shirt3.jpg" alt="" > </div>
+            <article>
+                
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae hic animi vel porro facilis ad illo incidunt qui beatae dicta, temporibus repellendus repellat ipsa quibusdam velit perspiciatis perferendis aspernatur quasi esse earum cum. Reprehenderit, nulla ut voluptatem reiciendis veritatis, vitae id corrupti molestiae nihil, alias illum incidunt laudantium doloribus sint suscipit ab molestias repudiandae ipsum numquam ex? Neque in magni soluta consequuntur accusantium aspernatur quod beatae sequi itaque fuga debitis vero qui maiores doloremque, aliquid culpa placeat cumque quas impedit alias odio eos fugit nesciunt dignissimos? Dolorum impedit ipsa vitae non itaque voluptatem totam. Temporibus illo fuga saepe. Minima, illum!
+            </article>
+            <article>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores officiis consequuntur ratione. Quisquam quidem iste culpa, laborum, consequuntur doloribus cum reiciendis voluptas necessitatibus nostrum, similique vero. Quae tenetur, dolorem beatae fugiat delectus sit ipsa quibusdam alias rem similique, veniam voluptate amet explicabo doloremque vel nulla natus facilis illo quam maxime soluta, eligendi voluptatibus? Atque, nobis repellendus nostrum error consectetur, veritatis vitae corrupti delectus at inventore incidunt. Quisquam sequi optio dolores veritatis commodi vitae dolore libero eum nesciunt? Commodi voluptates esse, quisquam asperiores consequatur harum aut cupiditate molestiae vel, ex consectetur temporibus, culpa earum illo non necessitatibus nam assumenda qui quibusdam.
+            </article>
+        </section>
+
+        <section class="row justify-content-around">
+        <div class="card col-md-3" style="width: 18rem;">
+            <img src="tee-shirt5.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+        <div class="card col-md-3" style="width: 18rem;">
+            <img src="tee-shirt4.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+        <div class="card col-md-3" style="width: 18rem;">
+            <img src="tee-shirt2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </section>
     </main>
 
 
